2dd94e108850b5dc7e083ebf66e44dcf373bde7f
DilanBrandix <101696989+DilanBrandix@users.noreply.github.com>
2023-09-05 13:38:05 +0530

    Login and user creation completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://upendramalindu116:xOIyyEfzVL3ERnhR@cluster0.e9lq4b1.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
